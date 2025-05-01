62d47bb5915ca9c2b3c2d50456c6c16c4f4fa5ce
Marcglacier32 <mark.thiongo@student.moringaschool.com>
2025-05-01 15:29:45 +0300

    completed init.py and department.py

--- lib/debug.py
+++ lib/debug.py
@@ -1,9 +1,24 @@
-#!/usr/bin/env python3
-#lib/testing/debug.py
-
+# lib/debug.py
 from __init__ import CONN, CURSOR
 from department import Department
-
 import ipdb
+
+Department.drop_table()
+Department.create_table()
+
+payroll = Department.create("Payroll", "Building A, 5th Floor")
+print(payroll)
+
+hr = Department.create("Human Resources", "Building C, East Wing")
+print(hr)
+
+# Update HR department
+hr.name = "HR"
+hr.location = "Building F, 10th Floor"
+hr.update()
+print(hr)
+
+# Optional: delete payroll department
+payroll.delete()
 
 ipdb.set_trace()
